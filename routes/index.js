3ed2cb328531090b8afcb16cb60174e8506117b8
Leroy Salih <leroy.salih@gmail.com>
2024-05-27 04:55:58 +0300

    Added .env var

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express from Coolio' });
+  res.render('index', { title: `Express from Coolio on ${process.env.SERVER}` });
 });
 
 module.exports = router;
